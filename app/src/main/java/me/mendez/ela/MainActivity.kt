408b61ea781fc364e280f6214783e0d36486a169
Méndez <47764656+El-Mendez@users.noreply.github.com>
2023-09-06 10:15:19 -0600

    refactor done

--- app/src/main/java/me/mendez/ela/MainActivity.kt
+++ app/src/main/java/me/mendez/ela/MainActivity.kt
@@ -1,18 +1,11 @@
 package me.mendez.ela
 
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.VpnService
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.collectAsState
-import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
@@ -22,42 +15,20 @@ import me.mendez.ela.services.NotificationService
 import me.mendez.ela.settings.ElaSettings
 import me.mendez.ela.ui.theme.ElaTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import me.mendez.ela.ui.screens.*
+import me.mendez.ela.ui.screens.DailyBlocksScreen
+import me.mendez.ela.ui.screens.MainScreen
 import me.mendez.ela.ui.screens.chat.ChatScreen
 import me.mendez.ela.ui.screens.chat.ChatViewModel
 import me.mendez.ela.ui.screens.settings.SettingsScreen
-import me.mendez.ela.vpn.ElaVpn
+import me.mendez.ela.ui.screens.settings.SettingsViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var notificationService: NotificationService
-    private val TAG = "MAIN"
 
     @Inject
     lateinit var elaSettingsStore: DataStore<ElaSettings>
-
-    private val waitForIntentResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        Log.d(TAG, "vpn permission state $it")
-        if (it.resultCode == Activity.RESULT_OK) {
-            startVpn()
-        } else {
-            setVpnStatus(false)
-        }
-    }
-
-    private val requestPermissions = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        if (it) {
-            Log.i(TAG, "user notification permission accepted")
-            tryStartVpn()
-        } else {
-            Log.i(TAG, "user rejected permissions. Turn off vpn")
-            setVpnStatus(false)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,6 +39,21 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val chatViewModel = viewModel<ChatViewModel>()
                 val elaSettings = elaSettingsStore.data.collectAsState(initial = ElaSettings.default()).value
+
+                val settingsViewModel = viewModel<SettingsViewModel>()
+                val intentContract = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartActivityForResult(),
+                    onResult = { settingsViewModel.onVpnPermissionResponse(it, this@MainActivity) }
+                )
+                val stringContract = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = {
+                        settingsViewModel.onNotificationPermissionResponse(
+                            it,
+                            this@MainActivity,
+                            intentContract
+                        )
+                    })
 
                 NavHost(navController = navController, startDestination = "home") {
                     composable("home") {
@@ -93,36 +79,19 @@ class MainActivity : ComponentActivity() {
                             chatViewModel.messages,
                             chatViewModel.calculatingResponse.value,
                             onSubmit = chatViewModel::sendMessage,
-                            onReturn = {
-                                navController.popBackStack()
-                            }
+                            onReturn = navController::popBackStack
+
                         )
                     }
 
                     composable("settings") {
                         SettingsScreen(
-                            onReturn = { navController.popBackStack() },
+                            onReturn = navController::popBackStack,
                             settingsStore = elaSettingsStore,
                             startVpn = {
-                                if (!it) {
-                                    Log.d(TAG, "stopping vpn")
-                                    stopVpn()
-                                    return@SettingsScreen
-                                }
-
-                                setVpnStatus(true)
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                                    if (!notificationPermission()) {
-                                        Log.i(TAG, "notifications permissions not granted!")
-                                        askForNotificationPermissions()
-                                        return@SettingsScreen
-                                    }
-                                }
-
-                                Log.d(TAG, "notification permissions is already granted")
-                                tryStartVpn()
+                                settingsViewModel.trySetVpnStatus(it, this@MainActivity, stringContract, intentContract)
                             },
-                            onUpdate = { restartVpn() }
+                            onUpdate = { settingsViewModel.restartVpn(this@MainActivity) }
                         )
                     }
 
@@ -134,67 +103,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun askForNotificationPermissions() {
-        requestPermissions.launch(
-            android.Manifest.permission.POST_NOTIFICATIONS
-        )
-    }
-
-    private fun setVpnStatus(on: Boolean) {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                elaSettingsStore.updateData { old ->
-                    old.copy(vpnRunning = on)
-                }
-            }
-        }
-    }
-
-    private fun tryStartVpn() {
-        setVpnStatus(true)
-
-        val status = VpnService.prepare(this)
-        if (status != null) {
-            Log.d(TAG, "waiting for service $status")
-            waitForIntentResult.launch(status)
-            return
-        }
-
-        startVpn()
-    }
-
-    private fun startVpn() {
-        Intent(applicationContext, ElaVpn::class.java).also { intent ->
-            intent.action = ElaVpn.Commands.START.toString()
-            startService(intent)
-        }
-    }
-
-    private fun stopVpn() {
-        setVpnStatus(false)
-        Intent(applicationContext, ElaVpn::class.java).also { intent ->
-            intent.action = ElaVpn.Commands.STOP.toString()
-            startService(intent)
-        }
-    }
-
-    private fun restartVpn() {
-        setVpnStatus(true)
-        Intent(applicationContext, ElaVpn::class.java).also { intent ->
-            intent.action = ElaVpn.Commands.RESTART.toString()
-            startService(intent)
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun notificationPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.POST_NOTIFICATIONS,
-        ) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onStop() {
--- app/src/main/java/me/mendez/ela/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/me/mendez/ela/ui/screens/settings/SettingsViewModel.kt
@@ -1,23 +1,149 @@
 package me.mendez.ela.ui.screens.settings
 
+import android.app.Activity
 import android.content.Context
-import androidx.compose.runtime.collectAsState
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.VpnService
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ActivityContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import me.mendez.ela.remote.ChatApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.mendez.ela.settings.ElaSettings
+import me.mendez.ela.vpn.ElaVpn
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val dataStore: DataStore<ElaSettings>,
 ) : ViewModel() {
+    private val TAG = "SETTINGS"
+
     val state: Flow<ElaSettings>
         get() = dataStore.data
 
 
+    fun trySetVpnStatus(
+        it: Boolean,
+        context: Context,
+        stringContract: ActivityResultLauncher<String>,
+        intentContract: ActivityResultLauncher<Intent>
+    ) {
+        if (!it) {
+            Log.d(TAG, "stopping vpn")
+            stopVpn(context)
+            return
+        }
+
+        setVpnStatus(true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!notificationPermission(context)) {
+                Log.i(TAG, "notification permissions not granted!")
+                askForNotificationPermissions(stringContract)
+                return
+            }
+        }
+
+        Log.d(TAG, "notification permissions are already granted")
+        tryStartVpn(context, intentContract)
+    }
+
+    private fun setVpnStatus(on: Boolean) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                dataStore.updateData { old ->
+                    old.copy(vpnRunning = on)
+                }
+            }
+        }
+    }
+
+    private fun tryStartVpn(context: Context, intentContract: ActivityResultLauncher<Intent>) {
+        setVpnStatus(true)
+
+        val status = VpnService.prepare(context)
+        if (status != null) {
+            Log.d(TAG, "waiting for service $status")
+            askForVpnPermission(intentContract, status)
+            return
+        }
+        startVpn(context)
+    }
+
+
+    private fun startVpn(context: Context) {
+        setVpnStatus(true)
+        Intent(context, ElaVpn::class.java).also { intent ->
+            intent.action = ElaVpn.Commands.START.toString()
+            context.startService(intent)
+        }
+    }
+
+    private fun stopVpn(context: Context) {
+        setVpnStatus(false)
+        Intent(context, ElaVpn::class.java).also { intent ->
+            intent.action = ElaVpn.Commands.STOP.toString()
+            context.startService(intent)
+        }
+    }
+
+    fun restartVpn(context: Context) {
+        setVpnStatus(true)
+        Intent(context, ElaVpn::class.java).also { intent ->
+            intent.action = ElaVpn.Commands.RESTART.toString()
+            context.startService(intent)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun notificationPermission(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.POST_NOTIFICATIONS,
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun askForNotificationPermissions(stringContract: ActivityResultLauncher<String>) {
+        stringContract.launch(
+            android.Manifest.permission.POST_NOTIFICATIONS
+        )
+    }
+
+    fun onNotificationPermissionResponse(
+        allowed: Boolean,
+        context: Context,
+        intentContract: ActivityResultLauncher<Intent>
+    ) {
+        if (allowed) {
+            Log.i(TAG, "user notification permission accepted")
+            tryStartVpn(context, intentContract)
+        } else {
+            Log.i(TAG, "user rejected permissions. Turn off vpn")
+            setVpnStatus(false)
+        }
+    }
+
+    private fun askForVpnPermission(activityContract: ActivityResultLauncher<Intent>, intent: Intent) {
+        activityContract.launch(intent)
+    }
+
+    fun onVpnPermissionResponse(result: ActivityResult, context: Context) {
+        Log.d(TAG, "vpn permission state $result")
+        if (result.resultCode == Activity.RESULT_OK) {
+            startVpn(context)
+        } else {
+            setVpnStatus(false)
+        }
+    }
 }

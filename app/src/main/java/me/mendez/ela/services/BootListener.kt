6a36405c985db83280f0c362927358550cde173b
Méndez <47764656+El-Mendez@users.noreply.github.com>
2023-10-28 20:19:44 -0600

    on app update restart vpn

--- app/src/main/java/me/mendez/ela/services/BootListener.kt
+++ app/src/main/java/me/mendez/ela/services/BootListener.kt
@@ -18,26 +18,44 @@ class BootListener : BroadcastReceiver() {
             return
         }
 
-        if (Intent.ACTION_BOOT_COMPLETED != intent.action) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            onBoot(context)
+        } else if (Intent.ACTION_MY_PACKAGE_REPLACED == intent.action) {
+            onUpdate(context)
+        } else {
             Log.i(TAG, "Not a boot. Ignoring intent.")
-            return
+        }
+    }
+
+    private fun onUpdate(context: Context) {
+        val dataStore = ElaSettingsModule.provideElaSettingsStore(context)
+        Log.d(TAG, "app updated!")
+        val state = runBlocking {
+            dataStore.data.first()
         }
 
+        if (state.vpnRunning) {
+            Log.i(TAG, "restarting vpn after app update")
+            ElaVpnService.sendStart(context)
+        } else {
+            Log.i(TAG, "App updated, nothing to do")
+        }
+    }
+
+    private fun onBoot(context: Context) {
         val dataStore = ElaSettingsModule.provideElaSettingsStore(context)
 
-        Log.i(TAG, "Boot event received")
+        Log.i(TAG, "Running phone booted code")
         runBlocking {
             val state = dataStore.data.first()
 
             if (state.startOnBoot) {
-                Log.i(TAG, "Start VPN because of settings")
+                Log.d(TAG, "Start VPN because of settings")
                 ElaVpnService.sendStart(context)
             } else if (state.vpnRunning || !state.ready) {
-                Log.i(TAG, "Reset VPN status to off")
+                Log.d(TAG, "Reset VPN status to off")
                 ElaVpnService.showRunning(dataStore, running = false, ready = true)
             }
         }
-
-
     }
 }
